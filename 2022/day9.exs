09fb2ee2737d66d986b5e92f20691388f34423b7
Zac Holden <zacholden@gmail.com>
2022-12-10 17:32:07 -0800

    Day 9 2022 part 2

--- 2022/day9.exs
+++ 2022/day9.exs
@@ -2,125 +2,95 @@ defmodule Day9 do
   @input File.stream!("day9.txt")
          |> Stream.map(&String.trim/1)
          |> Stream.map(&String.split/1)
-         |> Enum.map(fn [dir, amount] -> {dir, String.to_integer(amount)} end)
+         |> Stream.map(fn [dir, amount] -> String.duplicate(dir, String.to_integer(amount)) end)
+         |> Enum.flat_map(&String.codepoints/1)
 
   def part1 do
-    Enum.reduce(@input, [{{0, 0}, {0, 0}}], fn {dir, amount}, acc ->
-      [{head, tail} | _rest] = acc
+    knots = [Enum.map(0..1, fn _ -> {0, 0} end)]
 
-      moves = move(dir, amount, head, tail)
+    Enum.reduce(@input, knots, fn dir, acc ->
+      [{x, y} | rest] = hd(acc)
 
-      Enum.reduce(moves, acc, fn move, inner_acc -> [move | inner_acc] end)
+      head = move(dir, {x, y})
+
+      [propogate(rest, [head]) | acc]
     end)
-    |> Enum.map(fn {_head, tail} -> tail end)
+    |> Enum.map(&List.last/1)
     |> Enum.uniq()
     |> Enum.count()
   end
 
-  def move("U", amount, {hx, hy}, {tx, ty}) do
-    Enum.reduce(1..amount, [{{hx, hy}, {tx, ty}}], fn _, acc ->
-      [{{hx, hy}, {tx, ty}} | _tail] = acc
+  def part2 do
+    knots = [Enum.map(0..9, fn _ -> {0, 0} end)]
 
-      new_hy = hy + 1
+    Enum.reduce(@input, knots, fn dir, acc ->
+      [{x, y} | rest] = hd(acc)
 
-      new_tail =
-        cond do
-          new_hy - 2 == ty && hx == tx ->
-            {tx, ty + 1}
+      head = move(dir, {x, y})
 
-          new_hy - 2 == ty && hx < tx ->
-            {tx - 1, ty + 1}
-
-          new_hy - 2 == ty && hx > tx ->
-            {tx + 1, ty + 1}
-
-          true ->
-            {tx, ty}
-        end
-
-      [{{hx, new_hy}, new_tail} | acc]
+      [propogate(rest, [head]) | acc]
     end)
-    |> Enum.take(amount)
-    |> Enum.reverse()
+    |> Enum.map(&List.last/1)
+    |> Enum.uniq()
+    |> Enum.count()
   end
 
-  def move("R", amount, {hx, hy}, {tx, ty}) do
-    Enum.reduce(1..amount, [{{hx, hy}, {tx, ty}}], fn _, acc ->
-      [{{hx, hy}, {tx, ty}} | _tail] = acc
-      new_hx = hx + 1
+  def propogate([], result), do: Enum.reverse(result)
 
-      new_tail =
-        cond do
-          new_hx - 2 == tx && hy == ty ->
-            {tx + 1, ty}
+  # There are 18 options of where the knot you are following could be because the knots can move diagonally
+  # to follow the head, in the cond they start from directy two squares above and move clockwise
+  def propogate([{tx, ty} | rest], result) do
+    {hx, hy} = hd(result)
 
-          new_hx - 2 == tx && hy < ty ->
-            {tx + 1, ty - 1}
+    new_tail = follow({hx, hy}, {tx, ty})
 
-          new_hx - 2 == tx && hy > ty ->
-            {tx + 1, ty + 1}
-
-          true ->
-            {tx, ty}
-        end
-
-      [{{new_hx, hy}, new_tail} | acc]
-    end)
-    |> Enum.take(amount)
-    |> Enum.reverse()
+    propogate(rest, [new_tail | result])
   end
 
-  def move("L", amount, {hx, hy}, {tx, ty}) do
-    Enum.reduce(1..amount, [{{hx, hy}, {tx, ty}}], fn _, acc ->
-      [{{hx, hy}, {tx, ty}} | _tail] = acc
-      new_hx = hx - 1
+  def move("U", {x, y}), do: {x, y + 1}
+  def move("R", {x, y}), do: {x + 1, y}
+  def move("L", {x, y}), do: {x - 1, y}
+  def move("D", {x, y}), do: {x, y - 1}
 
-      new_tail =
-        cond do
-          new_hx + 2 == tx && hy == ty ->
-            {tx - 1, ty}
+  def follow({hx, hy}, {tx, ty}) do
+    cond do
+      hx == tx && hy - 2 == ty ->
+        # up
+        {tx, ty + 1}
 
-          new_hx + 2 == tx && hy < ty ->
-            {tx - 1, ty - 1}
+      (hx > tx && hy - 2 == ty) || (hx - 2 == tx && hy > ty) ->
+        # right up
+        {tx + 1, ty + 1}
 
-          new_hx + 2 == tx && hy > ty ->
-            {tx - 1, ty + 1}
+      hx - 2 == tx && hy == ty ->
+        # right
+        {tx + 1, ty}
 
-          true ->
-            {tx, ty}
-        end
+      (hx - 2 == tx && hy < ty) || (hx > tx && hy + 2 == ty) ->
+        # right down
+        {tx + 1, ty - 1}
 
-      [{{new_hx, hy}, new_tail} | acc]
-    end)
-    |> Enum.take(amount)
-    |> Enum.reverse()
-  end
+      hx == tx && hy + 2 == ty ->
+        # down
+        {tx, ty - 1}
 
-  def move("D", amount, {hx, hy}, {tx, ty}) do
-    Enum.reduce(1..amount, [{{hx, hy}, {tx, ty}}], fn _, acc ->
-      [{{hx, hy}, {tx, ty}} | _tail] = acc
-      new_hy = hy - 1
+      (hx < tx && hy + 2 == ty) || (hx + 2 == tx && hy < ty) ->
+        # left down
+        {tx - 1, ty - 1}
 
-      new_tail =
-        cond do
-          new_hy + 2 == ty && hx == tx ->
-            {tx, ty - 1}
+      hx + 2 == tx && hy == ty ->
+        # left
+        {tx - 1, ty}
 
-          new_hy + 2 == ty && hx < tx ->
-            {tx - 1, ty - 1}
+      (hx + 2 == tx && hy > ty) || (hx < tx && hy - 2 == ty) ->
+        # up left
+        {tx - 1, ty + 1}
 
-          new_hy + 2 == ty && hx > tx ->
-            {tx + 1, ty - 1}
-
-          true ->
-            {tx, ty}
-        end
-
-      [{{hx, new_hy}, new_tail} | acc]
-    end)
-    |> Enum.take(amount)
-    |> Enum.reverse()
+      true ->
+        {tx, ty}
+    end
   end
 end
 
 Day9.part1() |> IO.inspect()
+Day9.part2() |> IO.inspect()
