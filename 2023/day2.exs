9a260ed21516e9dae3e2b6ebd241a7ec278f2d82
Zac Holden <zacholden@gmail.com>
2023-12-02 09:43:53 -0800

    2023 day 2 parts 1 & 2

---
+++ 2023/day2.exs
@@ -0,0 +1,35 @@
+input =
+  File.read!("day2.txt")
+  |> String.replace("Game ", "")
+  |> String.split("\n", trim: true)
+  |> Enum.map(fn str -> String.split(str, ": ") end)
+  |> Enum.map(fn [a, b] ->
+    {String.to_integer(a),
+     String.split(b, "; ")
+     |> Enum.map(fn str ->
+       String.split(str, ", ")
+       |> Map.new(fn str ->
+         String.split(str)
+         |> then(fn [amount, color] -> {color, String.to_integer(amount)} end)
+       end)
+     end)}
+  end)
+
+merger = fn left, right -> Map.merge(left, right, fn _k, v1, v2 -> max(v1, v2) end) end
+
+totals = Enum.map(input, fn {k, v} -> {k, Enum.reduce(v, %{}, merger)} end)
+
+# Part 1
+limits = %{"red" => 12, "green" => 13, "blue" => 14}
+
+Enum.filter(totals, fn {_k, v} ->
+  Enum.all?(v, fn {color, value} -> value <= limits[color] end)
+end)
+|> Enum.reduce(0, fn {k, _cubes}, acc -> acc + k end)
+|> IO.inspect()
+
+# Part 2
+Enum.reduce(totals, 0, fn {_game_id, cubes}, acc ->
+  acc + (Map.values(cubes) |> Enum.product())
+end)
+|> IO.inspect()
