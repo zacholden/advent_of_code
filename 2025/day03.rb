baf2621e9be5cfb08e4369368939ad079ad621d1
Zac Holden <zacholden@gmail.com>
2025-12-03 14:23:50 -0800

    Generalize solution for 2025 day 3

--- 2025/day03.rb
+++ 2025/day03.rb
@@ -1,34 +1,14 @@
 input = File.read("day03.txt").split.map { |str| str.split('').map(&:to_i) }
 
-part1 = input.sum do |arr|
-  first = arr[..-2].max
-  first_index = arr.index(first)
-  second = arr[(first_index + 1)..].max
+def find_high_number(arr, length)
+  return 0 if length.zero?
 
-  first * 10 + second
+  max = arr[..-length].max
+  index = arr.index(max) + 1
+
+  max * 10**(length-1) + find_high_number(arr[index..], length - 1)
 end
 
-LENGTH = 12
+puts input.sum { |arr| find_high_number(arr, 2) }
 
-def build_high_number(arr, result = [])
-  return result.join.to_i if result.length == LENGTH
-
-  max_in_bounds = arr.take(arr.length - LENGTH + 1 + result.length).max
-  head = arr.shift
-  result << head if head == max_in_bounds
-
-  build_high_number(arr, result)
-end
-
-puts part1
-
-part2 = input.sum do |arr|
-  possible_starts = arr.take(arr.length - LENGTH + 1)
-  first = possible_starts.max
-
-  indexes = possible_starts.filter_map.with_index { |i, idx| i == first && idx}
-
-  indexes.map { |index| build_high_number(arr.drop(index)) }.max
-end
-
-puts part2
+puts input.sum { |arr| find_high_number(arr, 12) }
