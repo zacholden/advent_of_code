48e3be858c5c1f19c98c816caa59468324b12fac
Zac Holden <zacholden@gmail.com>
2025-12-05 08:38:39 -0800

    Simplify 2025 day 5 part 2

--- 2025/day05.rb
+++ 2025/day05.rb
@@ -5,24 +5,16 @@ ranges = input.first.split.map { |str| str.split('-') }.map { |a, b| a.to_i..b.t
 puts input.last.split.map(&:to_i).count { |i| ranges.any? { |r| r.cover?(i) } }
 
 i = 0
-combinations = 0
 
 loop do
-  if i == ranges.length - 1
-    break if combinations.zero?
-
-    combinations = 0
-    i = 0
-  end
+  break if i == ranges.length - 1
 
   r1 = ranges[i]
   r2 = ranges[i + 1]
 
   next i += 1 unless r1.overlap?(r2)
 
-  combinations += 1
   last = [r1.last, r2.last].max
-
   ranges.delete_at(i)
   ranges[i] = r1.first..last
 end
